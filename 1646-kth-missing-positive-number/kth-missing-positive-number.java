2f3cc386b63a959c2fbe88e9cd0bfd722d873aa9
Rishi Raj Khanna <rishirajkhanna2511@gmail.com>
2026-08-11 14:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (40.13%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,53 +1,24 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
 
-       for(int i=0;i<arr.length;i++)
-       {
-        if(arr[i]<=k)
-        {
-            k+=1;
-        }
-        else
-        {
-            return k;
-        }
-       }
-       
-       return k;
+        int s=0;
+        int e=arr.length-1;
+        int mid=0;
 
-        // int s=0;
-        // int e=arr.length-1;
-        // int mid=0;
-        // int ans=0;
-
-        // while(s<=e)
-        // {
-        //     mid=s+(e-s)/2;
+        while(s<=e)
+        {
+            mid=s+(e-s)/2;
             
-        //     if(arr[mid]-mid==k)
-        //     {
-        //         ans=mid;
-        //         break;
-        //     }
-        //     else if(arr[mid]-mid>k)
-        //     {
-        //         ans=mid;
-        //         e=mid-1;
-        //     }
-        //     else if((arr[mid]-mid)<k)
-        //     {
-        //         s=mid+1;
-        //     }
-        // }
+            if((arr[mid]-mid-1)<k)
+            {
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
+            }
+        }
 
-        // if(s>e)
-        // {
-        //    return arr[arr.length-1]+k;
-        // }
-        // else if(mid>0)
-        // {
-        //    return arr[mid-1]+((arr[mid]-mid)-(arr[mid-1]-(mid-1)));
-        // }
-        // return ((arr[mid]-mid)-(arr[mid-1]-(mid-1)));
+        return k+e+1             ; // arr[e]+more -> more = k-(arr[e]-e-1) 
     }
 }
