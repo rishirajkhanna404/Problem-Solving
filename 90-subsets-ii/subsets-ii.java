2597e6f247b511311807bd9ee48a051e46a18863
Rishi Raj Khanna <rishirajkhanna2511@gmail.com>
2026-08-12 13:24:12 +0530

    Time: 3 ms (55.10%) | Memory: 45 MB (77.15%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,29 +1,26 @@
 class Solution {
+    public void rec(int[] nums,List<List<Integer>> ans,List<Integer> list,int idx)
+    {
+        ans.add(new ArrayList<>(list));
+
+        for(int i=idx;i<nums.length;i++)
+        {
+            if(i>idx && nums[i]==nums[i-1])
+            {
+                continue;
+            }
+
+            list.add(nums[i]);
+            rec(nums,ans,list,i+1);
+            list.remove(list.size()-1);
+        }
+    }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans=new ArrayList<>();
-        Set<List<Integer>> subAns=new HashSet<>();
-        int n=nums.length;
-
-        for(int i=0;i<Math.pow(2,n);i++)
-        {
-            List<Integer> list=new ArrayList<>();
-            
-            for(int j=0;j<n;j++)
-            {
-                int x=1<<(n-1-j);
-                if((i&x)!=0)
-                {
-                    list.add(nums[j]);
-                }
-            }
-          subAns.add(list);
-        }
-
-        for(List<Integer> y:subAns)
-        {
-            ans.add(y);
-        }
+        List<Integer> list=new ArrayList<>();
+        
+        rec(nums,ans,list,0);
         return ans;
     }
 }
